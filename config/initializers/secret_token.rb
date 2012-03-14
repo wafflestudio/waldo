42b68361c005b301ffa875687baf290e43bb5adc
JoonHyeong Ahn <rkdrnf@junhyeong-ui-MacBook-Pro.local>
2012-03-15 04:57:32 +0900

    프로젝트 생성

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waldo::Application.config.secret_token = '9a0535e1e51bfadfb1876516ad5dfd640ac57f396d84f0ffc0aae530fadd1b53399bd5283ebe77c5f30ba7765ce55d32214974f6757473be998d8537297539b4'
